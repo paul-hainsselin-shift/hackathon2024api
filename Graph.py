a7b0bd11c6f771a00adbabb7baf87a952699a7e2
paul-hainsselin-shift <117901300+paul-hainsselin-shift@users.noreply.github.com>
2024-02-01 15:44:58 +0100

    opk

---
+++ Graph.py
@@ -0,0 +1,81 @@
+import json
+
+class Node:
+    def __init__(self, id, name=None, color=None, label=None, title=None, shape=None, image=None, size=None, cost=None):
+        self.id = id
+        self.name = name
+        self.color = color
+        self.label = label
+        self.title = title
+        self.shape = shape
+        self.image = image
+        self.size = size
+        self.cost = cost
+
+    def to_dic(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "color": self.color,
+            "label": self.label,
+            "title": self.title,
+            "shape": self.shape,
+            "image": self.image,
+            "size": self.size,
+            "cost": self.cost
+        }
+
+
+class Edge:
+    def __init__(self, from_node, to_node, color=None, id=None):
+        self.from_node = from_node
+        self.to_node = to_node
+        self.color = color
+        self.id = id
+
+    def to_dic(self):
+        return {
+            "from": self.from_node.id,
+            "to": self.to_node.id,
+            "color": self.color,
+            "id":self.id
+        }
+class Graph:
+    def __init__(self):
+        self.nodes = []
+        self.edges = []
+
+    def add_node(self, node):
+        self.nodes.append(node)
+
+    def add_edge(self, edge):
+        self.edges.append(edge)
+
+    def get_node(self, id):
+        for node in self.nodes:
+            if node.id == id:
+                return node
+
+    def to_dic(self):
+        res = dict()
+        res["node"] = [i.to_dic() for i in self.nodes]
+        res["edge"] = [i.to_dic() for i in self.edges]
+        return res
+
+
+# Example Usage:
+if __name__ == "__main__":
+
+    graph = Graph()
+    node1 = Node(id="a")
+    node2 = Node(id="b")
+    graph.add_node(node1)
+    graph.add_node(node2)
+
+
+    edge1 = Edge(node1,node2)
+    graph.add_edge(edge1)
+
+
+    # Print nodes and edges
+    print(graph.to_dic())
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import os
 from azure.core.exceptions import AzureError
 from azure.cosmos import CosmosClient, PartitionKey
+from Graph import Graph, Node, Edge
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ gremlin_endpoint = 'wss://team17.gremlin.cosmos.azure.com:443/'
 auth_key = os.environ.get('primary_db_key')
 auth_key = "f71da6Q1B8wOID71BIUiEzJMSyoTFpZxrDjTPJJsRszW3z3vIgCkQxHDs94wnMecR4g1XFoVw61NACDbRxXCEg=="
 
-db_name = 'team17'
+db_name = 'graphdb'
 container_name = 'Network'
 
 client = CosmosClient(URI, credential=auth_key)
@@ -20,13 +21,29 @@ container = database.get_container_client(container_name)
 # Endpoint to get a graph (simplified example)
 @app.route('/GetGraph', methods=['GET'])
 def get_graph():
-    items = list(container.query_items(
+    all_obj = list(container.query_items(
       query="SELECT * FROM c",
       enable_cross_partition_query=True
     ))
 
-    # Process fetched items if needed
-    return jsonify(items)
+    edges = [x for x in all_obj if "_isEdge" in x]
+    nodes = [x for x in all_obj if "_isEdge" not in x]
+
+    my = Graph()
+
+    for n in nodes:
+        new_node = Node(id=n["id"], label=n["label"])
+        my.add_node(new_node)
+
+    for e in edges:
+        node1 = my.get_node(e["_sink"])
+        node2 = my.get_node(e["_vertexId"])
+        new_edge = Edge(node1, node2, "red", 'id')
+        my.add_edge(new_edge)
+
+    dd = my.to_dic()
+    print(dd)
+    return jsonify(dd)
 
 
 # Endpoint to get the shortest path between two nodes
